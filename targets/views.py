c7e7622c52cc5f78527b76a14961321b48373e4a
Jonathan Powers <bigblackrobot@gmail.com>
2025-11-04 09:08:07 -0600

    Fix missing login_required import

--- targets/views.py
+++ targets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.db import connection
 from datetime import date, datetime, timedelta
